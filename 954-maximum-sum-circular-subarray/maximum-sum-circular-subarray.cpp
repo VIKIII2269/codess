dc62d988f01c755c2355551842060a8a41c65792
TeraWrist2692 <140892495+VIKIII2269@users.noreply.github.com>
2024-01-26 19:49:08 +0530

    Time: 51 ms (56.38%) | Memory: 42.3 MB (13.32%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,17 +1,16 @@
-#include <vector>
-#include <algorithm>
+
 
 class Solution {
 public:
-    int maxSubarraySumCircular(std::vector<int>& arr) {
+    int maxSubarraySumCircular(vector<int>& arr) {
         int n = arr.size();
 
         
         int maxSubarraySum = arr[0];
         int currentMax = arr[0];
         for (int i = 1; i < n; i++) {
-            currentMax = std::max(arr[i], currentMax + arr[i]);
-            maxSubarraySum = std::max(maxSubarraySum, currentMax);
+            currentMax = max(arr[i], currentMax + arr[i]);
+            maxSubarraySum = max(maxSubarraySum, currentMax);
         }
 
         
@@ -20,8 +19,8 @@ public:
         int currentMin = arr[0];
         for (int i = 1; i < n; i++) {
             totalSum += arr[i];
-            currentMin = std::min(arr[i], currentMin + arr[i]);
-            minSubarraySum = std::min(minSubarraySum, currentMin);
+            currentMin = min(arr[i], currentMin + arr[i]);
+            minSubarraySum = min(minSubarraySum, currentMin);
         }
 
        
@@ -30,6 +29,6 @@ public:
         }
 
         
-        return std::max(maxSubarraySum, totalSum - minSubarraySum);
+        return max(maxSubarraySum, totalSum - minSubarraySum);
     }
 };
