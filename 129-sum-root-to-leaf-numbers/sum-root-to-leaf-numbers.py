4449e10a8af5fa112bbf02df25e4e46695f69f66
TeraWrist2692 <140892495+VIKIII2269@users.noreply.github.com>
2024-10-03 00:16:32 +0530

    Time: 38 ms (37.41%) | Memory: 16.5 MB (33.19%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -1,13 +1,9 @@
 class Solution:
-    def depth(self, root) -> int:
-        if not root:
-            return 0
-        return max(self.depth(root.left), self.depth(root.right)) + 1
-
+   
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         s = 0 
 
-        def solve(root, d, v):
+        def solve(root, v):
             nonlocal s 
             if not root:
                 return
@@ -21,9 +17,8 @@ class Solution:
                 return
 
            
-            solve(root.left, d - 1, v)
-            solve(root.right, d - 1, v)
+            solve(root.left,  v)
+            solve(root.right, v)
 
-        d = self.depth(root) 
-        solve(root, d, 0)
+        solve(root, 0)
         return s
